ee0cd7b9489c2a646e3684c084063d09b878492b
sayburt <ixirmedya@gmail.com>
2026-03-09 00:40:21 +0300

    auth 404 hatası giderildi

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -15,7 +15,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
             priority: 1,
         },
         {
-            url: `${baseUrl}/auth`,
+            url: `${baseUrl}/login`,
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.5,
