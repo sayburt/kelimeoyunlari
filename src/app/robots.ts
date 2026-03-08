67d670fa24c7e2e6a33da5ebebfe3005bb49c9f5
sayburt <ixirmedya@gmail.com>
2026-03-08 12:55:40 +0300

    ads.txt dosyası yapılandırıldı

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,13 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: ['/api/', '/admin/', '/test-auth/'],
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: ['/', '/ads.txt'],
+                disallow: ['/api/', '/admin/', '/test-auth/'],
+            },
+        ],
         sitemap: 'https://www.kelimeoyunlari.tr/sitemap.xml',
     };
 }
